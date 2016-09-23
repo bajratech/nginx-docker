80a2943c87a911480dcc6c13a1b930bffd83acd4
Ankur Maharjan <akrmhrjn@MacMini.local>
2016-09-23 12:07:25 +0545

    updated renew certificate path

--- renew.sh
+++ renew.sh
@@ -6,9 +6,9 @@ count=`cat /home/keys/renew.txt | grep -c "No renewals were attempted"`
 if [ $count -ne 0 ]; then
      echo "No renewals were attempted." && exit 0
 else
-	cp /etc/letsencrypt/live/beta.sitegranny.com/cert.pem /home/keys/beta.sitegranny.com/
-	cp /etc/letsencrypt/live/beta.sitegranny.com/chain.pem /home/keys/beta.sitegranny.com/
-	cp /etc/letsencrypt/live/beta.sitegranny.com/privkey.pem /home/keys/beta.sitegranny.com/
-	cp /etc/letsencrypt/live/beta.sitegranny.com/fullchain.pem /home/keys/beta.sitegranny.com/
+	cp /etc/letsencrypt/live/sitegranny.com/cert.pem /home/keys/sitegranny.com/
+	cp /etc/letsencrypt/live/sitegranny.com/chain.pem /home/keys/sitegranny.com/
+	cp /etc/letsencrypt/live/sitegranny.com/privkey.pem /home/keys/sitegranny.com/
+	cp /etc/letsencrypt/live/sitegranny.com/fullchain.pem /home/keys/sitegranny.com/
     service nginx reload
 fi
